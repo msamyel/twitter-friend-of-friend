40f603078a8d447f0b97b38a45961c0a138dd914
Voslar Matej <msamyel@gmail.com>
2023-02-22 07:33:22 +0100

    add procedure to retrieve data from api

--- classes/patient_api_client.py
+++ classes/patient_api_client.py
@@ -10,28 +10,40 @@ from TwitterAPI import TwitterAPI
 
 class PatientApiClient:
     api_client_inner: TwitterAPI = None
-    current_request_count: int = 0
-    MAX_REQUEST_NUM: int = 15
+    current_request_count: dict = dict()
+    MAX_REQUEST_NUM: dict = dict()
     WAIT_MINUTES: int = 16  # add one extra minute just in case
-    
+
     is_authenticated: bool = False
 
     def __init__(self):
         load_dotenv()
+
+        self.MAX_REQUEST_NUM["following"] = 15
+        self.MAX_REQUEST_NUM["username"] = 300
+        self.reset_request_count()
+
         self.api_client_inner = TwitterAPI(os.getenv("TWITTER_API_KEY"),
                                            os.getenv("TWITTER_API_SECRET"),
                                            os.getenv("TWITTER_ACCESS_TOKEN"),
                                            os.getenv("TWITTER_ACCESS_SECRET"),
                                            api_version='2')
-        
+
         if self.api_client_inner is not None:
             self.is_authenticated = True
 
-    def handle_request_json(self, request, params):
-        if self.current_request_count >= self.MAX_REQUEST_NUM:
-            time.sleep(self.WAIT_MINUTES * 60)
-            self.current_request_count = 0
+    def reset_request_count(self):
+        self.current_request_count["following"] = 0
+        self.current_request_count["username"] = 0
 
-        self.current_request_count += 1
+    def handle_request_json(self, request_type, request, params=None):
+        assert (request_type in ["following", "username"])
+        if self.current_request_count[request_type] >= self.MAX_REQUEST_NUM[request_type]:
+            print(f"Reached API limit, sleeping for {self.WAIT_MINUTES} minutes...")
+            time.sleep(self.WAIT_MINUTES * 60)
+            print(f"Sleep finished, resuming work...")
+            self.reset_request_count()
+
+        self.current_request_count[request_type] += 1
         response = self.api_client_inner.request(request, params)
         return response.json()
---
+++ main.py
@@ -0,0 +1,134 @@
+import os
+import sys
+
+# local components
+from classes.patient_api_client import PatientApiClient
+
+
+def get_account_followers(api: PatientApiClient, account_id: int):
+    return get_account_relations(api, account_id, "followers")
+
+
+def get_account_following(api: PatientApiClient, account_id: int):
+    return get_account_relations(api, account_id, "following")
+
+
+def get_account_relations(api: PatientApiClient, account_id: int, type: str):
+    assert (type in ["followers", "following"])
+    follower_list = []
+    next_page_token = -1
+    params = dict()
+    params["max_results"] = 1000
+    while next_page_token is not None:
+        if (next_page_token != -1):
+            params["pagination_token"] = next_page_token
+        response_data = ""
+        try:
+            response = api.handle_request_json(request_type="following",
+                                               request=f'users/:{account_id}/{type}',
+                                               params=params)
+            if response is None:
+                return None
+
+            next_page_token = response["meta"].get("next_token")
+
+            print(response_data)
+
+            for item in response["data"]:
+                # we can learn this about the accounts: id, username (twitter @), name (display name)
+                # follower_list.append(f"{item['username']} ({item['id']}): {item['name']}")
+                # append only the handle
+                follower_list.append(item['username'])
+            print(f"-- \n Now the {type} list has length of {len(follower_list)} \n --")
+
+        except Exception as e:
+            sys.stderr.write(f"Error getting follower list for {account_id}: {e}, log: {response}\n")
+            return follower_list
+    return follower_list
+
+
+def get_username(api: PatientApiClient, user_id):
+    response = api.handle_request_json(request_type="username",
+                                       request=f'users/:{user_id}')
+    if response.get("errors"):
+        return None
+    return response.get("data").get("username")
+
+
+def read_source_ids_from_file():
+    if len(sys.argv) < 2:
+        print("Please provide a filename to read source id list from!")
+        return None
+
+    source_id_filename: str = sys.argv[1]
+    if not os.path.exists(source_id_filename):
+        print(f"Specified file {source_id_filename} does not exist.")
+        return None
+
+    with open(source_id_filename, 'r') as f:
+        source_ids = f.read().splitlines()
+
+    return source_ids
+
+
+def write_ids_with_usernames(id_username_pairs: dict):
+    with open("output/source_accounts.csv", 'w+') as f:
+        for user_id, username in id_username_pairs.items():
+            f.write(f"{user_id},{username}\n")
+
+
+def create_source_dictionary(api: PatientApiClient, source_lines: list):
+    source_dict: dict = dict()
+
+    for source_line in source_lines:
+        split_line = source_line.split(',')
+        if len(split_line) > 1 and split_line[1] and split_line[1].strip():
+            source_dict[split_line[0]] = split_line[1]
+        elif str.isnumeric(split_line[0]):
+            username: str = get_username(api, split_line[0])
+            if username and username.strip():
+                source_dict[split_line[0]] = username
+
+    return source_dict
+
+
+def write_results_to_file(api: PatientApiClient, file_handle, user_id: int, username: str):
+    following_list = get_account_following(api, user_id)
+    for following in following_list:
+        file_handle.write(f"{username},{following}\n")
+
+
+def create_output_folder_if_not_exists():
+    if not os.path.exists("output/"):
+        os.makedirs("output")
+
+#todo add option to only export twitter user ids, without usernames
+def main():
+    api: PatientApiClient = PatientApiClient()
+
+    if not api.is_authenticated:
+        sys.stderr.writelines("Could not authenticate TwitterAPI v2 client.\n"
+                              "Please check that .env file contains correct authentication data.\n")
+        return
+
+    source_lines = read_source_ids_from_file()
+    if source_lines is None:
+        return
+
+    create_output_folder_if_not_exists()
+
+    # get usernames for missing ids
+    source_dict = create_source_dictionary(api, source_lines)
+
+    #write id list with usernames, so that this can be reused later
+    #todo: only perform if specified in command line
+    write_ids_with_usernames(source_dict)
+
+    with open("output/result.csv", 'w+') as f:
+        f.write("source,target\n")
+        for (user_id, username) in source_dict.items():
+            write_results_to_file(api, f, user_id, username)
+
+
+if __name__ == '__main__':
+    main()
